67eee1f639078db84892d4d564d5084adc7e82c6
kozhibs <latukhingosha@gmail.com>
2026-07-08 21:22:16 +0300

    shit

--- flake.nix
+++ flake.nix
@@ -3,14 +3,15 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     import-tree.url = "github:vic/import-tree";
-    nh.url = "github:nix-community/nh";
     wrapper-modules.url = "github:BirdeeHub/nix-wrapper-modules";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    hjem.url = "github:feel-co/hjem";
   };
 
-   outputs =
-     inputs:
-     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-       systems = [ "x86_64-linux" ];
-       imports = [ (inputs.import-tree ./modules) ];
-     };
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+      imports = [ (inputs.import-tree ./modules) ];
+    };
 }
