fca52569840de7f30cb5d4253b4537ba14c5843a
kozhibs <latukhingosha@gmail.com>
2026-07-09 16:40:02 +0300

    updated everything

---
+++ modules/features/system/starship.nix
@@ -0,0 +1,53 @@
+{ self, inputs, ... }: {
+  flake.nixosModules.starship = { config, pkgs, lib, ... }: {
+    programs.starship.enable = true;
+
+    hjem.users.taito.xdg.configFile.'.config/starship.toml' = ''
+      add_newline = false;
+
+      format = """
+        $directory\
+        [at ](bold white)\
+        $username\
+        $git_branch\
+        $git_status\
+        $cmd_duration\
+        $all\
+        $line_break\
+        $character
+      """
+
+      [directory]
+      format = "[$path]($style)"
+      style = "white"
+      truncation_length = 4;
+      truncate_to_repo = true
+
+      [git_branch]
+      symbol = ""
+      format = "on [$symbol$branch]($style) "
+      style = "Bold White"
+
+      [git_status]
+      format = "([\\[$all_status$ahead_behind\\]]($style) )"
+      style = "white"
+      conflicted = "="
+      ahead = "↑"
+      behind = "↓"
+      diverged = "⇄"
+      modified = ""
+      untracked = ""
+      staged = ""
+      deleted = ""
+
+      [cmd_duration]
+      min_time = 2000
+      format = "took [$duration]($style) "
+      style = "bold white"
+
+      [character]
+      success_symbol = "[λ](white)"
+      error_symbol = "[λ](bold white)"
+    '';
+  };
+}
