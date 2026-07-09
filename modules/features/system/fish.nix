2a340b7d4e85991b1c6fd21d983432945aeb45c6
kozhibs <latukhingosha@gmail.com>
2026-07-09 16:41:31 +0300

    updated everything

--- modules/features/system/fish.nix
+++ modules/features/system/fish.nix
@@ -5,7 +5,6 @@
     hjem.users.taito.xdg.config.files."fish/config.fish".text = ''
       set -g fish_greeting ""
       alias ls "eza"
-      alias nh os switch "nos"
       starship init fish | source
     '';
   };
