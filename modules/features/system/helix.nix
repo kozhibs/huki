f20de6e86454fd9d696a9c211d51900647d085c3
kozhibs <latukhingosha@gmail.com>
2026-07-09 15:47:01 +0300

    updated helix

---
+++ modules/features/system/helix.nix
@@ -0,0 +1,27 @@
+{ self, inputs, ... }: {
+  flake.nixosModules.helix = { config, lib, pkgs, ... }: {
+
+    hjem.users.taito.xdg.config.files."helix/config.toml".text = ''
+      theme = "autumn"
+
+      [editor]
+      line-number = "relative"
+      cursorline = true
+      mouse = true
+      bufferline = "multiple"
+      auto-format = true
+      color-modes = true
+
+      [editor.cursor-shape]
+      insert = "bar"
+      normal = "block"
+      select = "underline"
+
+      [editor.indent-guides]
+      render = true
+
+      [editor.lsp]
+      display-messages = true
+    '';
+  };
+}
