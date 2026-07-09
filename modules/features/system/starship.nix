56f5ebebafbc2cf22898b45acca934e0d4ef4e82
kozhibs <latukhingosha@gmail.com>
2026-07-09 16:52:31 +0300

    updated everything

--- modules/features/system/starship.nix
+++ modules/features/system/starship.nix
@@ -2,7 +2,7 @@
   flake.nixosModules.starship = { config, pkgs, lib, ... }: {
     programs.starship.enable = true;
 
-    hjem.users.taito.xdg.config.files.".config/starship.toml" = ''
+    hjem.users.taito.xdg.config.file.".config/starship.toml" = ''
       add_newline = false;
 
       format = """
