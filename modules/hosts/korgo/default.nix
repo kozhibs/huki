1493cf4f228860652ace3feb9a4bea543351d302
kozhibs <latukhingosha@gmail.com>
2026-07-09 13:17:07 +0300

    BIG UDPATEgit add -A .git add -A .

--- modules/hosts/korgo/default.nix
+++ modules/hosts/korgo/default.nix
@@ -1,6 +1,6 @@
 { self, inputs, ... }: {
 
-  flake.nixosConfigurations = inputs.nixpkgs.lib.nixosSystem {
+  flake.nixosConfigurations.korgo = inputs.nixpkgs.lib.nixosSystem {
     system = "x86_64-linux";
     specialArgs = { inherit inputs self; };
     modules = [
