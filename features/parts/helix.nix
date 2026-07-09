493e6d47b58f2814220fe0827a859bce9160b404
kozhibs <latukhingosha@gmail.com>
2026-07-09 23:13:28 +0300

    global upd

--- features/parts/helix.nix
+++ features/parts/helix.nix
@@ -1,35 +1,42 @@
 { self, inputs, ... }: {
-  flake.nixosModules.helix = { config, lib, pkgs, ... }: {
+  flake.nixosModules.helix =
+    {
+      config,
+      lib,
+      pkgs,
+      ...
+    }:
+    {
 
-    hjem.users.taito.xdg.config.files."helix/config.toml".text = ''
-      theme = "autumn"
+      hjem.users.taito.xdg.config.files."helix/config.toml".text = ''
+        theme = "autumn"
 
-      [editor]
-      line-number = "relative"
-      cursorline = true
-      mouse = true
-      bufferline = "multiple"
-      auto-format = true
-      color-modes = true
+        [editor]
+        line-number = "relative"
+        cursorline = true
+        mouse = true
+        bufferline = "multiple"
+        auto-format = true
+        color-modes = true
 
-      [editor.cursor-shape]
-      insert = "bar"
-      normal = "block"
-      select = "underline"
+        [editor.cursor-shape]
+        insert = "bar"
+        normal = "block"
+        select = "underline"
 
-      [editor.indent-guides]
-      render = true
+        [editor.indent-guides]
+        render = true
 
-      [editor.lsp]
-      display-messages = true
-     '';
+        [editor.lsp]
+        display-messages = true
+      '';
 
-    hjem.users.taito.xdg.config.files."helix/languages.toml".text = ''
-      [[language]]
-      name = "nix"
-      language-servers = ["nil"]
-      formatter = { command = "nixfmt" }
-      auto-format = true
-    '';
-  };
+      hjem.users.taito.xdg.config.files."helix/languages.toml".text = ''
+        [[language]]
+        name = "nix"
+        language-servers = ["nil"]
+        formatter = { command = "nixfmt" }
+        auto-format = true
+      '';
+    };
 }
