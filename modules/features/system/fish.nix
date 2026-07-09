80081c01e2d442e9f60bf9409ef127b78e52c1c9
kozhibs <latukhingosha@gmail.com>
2026-07-09 18:49:21 +0300

    fish update

--- modules/features/system/fish.nix
+++ modules/features/system/fish.nix
@@ -4,6 +4,14 @@
 
     hjem.users.taito.xdg.config.files."fish/config.fish".text = ''
       set -g fish_greeting ""
+
+      set -g fish_color_command white
+      set -g fish_color_param brwhite
+      set -g fish_color_option brblack
+      set -g fish_color_quote white
+      set -g fish_color_error brwhite
+      set -g fish_color_operator brwhite
+      
       alias ls "eza"
       starship init fish | source
     '';
