100e07e89dea766cab86d54adc294bfe3d41aa56
kozhibs <latukhingosha@gmail.com>
2026-07-09 14:50:23 +0300

    niri update

--- modules/features/niri.nix
+++ modules/features/niri.nix
@@ -17,6 +17,12 @@
         xwayland-satellite.path = lib.getExe pkgs.xwayland-satellite;
 
         layout = {
+          keyboard = {
+            xkb = {
+              layout = "us,ru";
+              options = "grp:shift_alt_toggle:escape";
+            };
+          };
           gaps = 5;
           focus-ring = {
             width = 2;
@@ -29,19 +35,37 @@
           };
         };
 
-        keyboard = {
-          xkb = {
-            layout = "us,ru";
-            options = "grp:shift_alt_toggle:escape";
-          };
-        };
-
         binds = {
           "Mod+T".spawn-sh = lib.getExe pkgs.alacritty;
           "Mod+Q".close-window = [];
           "Mod+B".spawn-sh = lib.getExe pkgs.firefox;
           "Mod+D".spawn-sh = lib.getExe pkgs.fuzzel;
           "Mod+Shift+M".spawn-sh = "${lib.getExe self'.packages.myNoctalia} ipc call launcher toggle";
+
+          "Mod+1".focus-workspace = "w0";
+          "Mod+2".focus-workspace = "w1";
+          "Mod+3".focus-workspace = "w2";
+          "Mod+4".focus-workspace = "w3";
+          "Mod+5".focus-workspace = "w4";
+          "Mod+6".focus-workspace = "w5";
+          "Mod+7".focus-workspace = "w6";
+          "Mod+8".focus-workspace = "w7";
+          "Mod+9".focus-workspace = "w8";
+          "Mod+0".focus-workspace = "w9";
+
+          "Mod+Shift+1".move-column-to-workspace = "w0";
+          "Mod+Shift+2".move-column-to-workspace = "w1";
+          "Mod+Shift+3".move-column-to-workspace = "w2";
+          "Mod+Shift+4".move-column-to-workspace = "w3";
+          "Mod+Shift+5".move-column-to-workspace = "w4";
+          "Mod+Shift+6".move-column-to-workspace = "w5";
+          "Mod+Shift+7".move-column-to-workspace = "w6";
+          "Mod+Shift+8".move-column-to-workspace = "w7";
+          "Mod+Shift+9".move-column-to-workspace = "w8";
+          "Mod+Shift+0".move-column-to-workspace = "w9";
+
+          "Mod+F".maximize-column = [];
+          "Mod+Shift+F".fullscreen-window = [];
         };
       };
     };
