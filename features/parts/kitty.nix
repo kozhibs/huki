d2b744b8a3a261763dfcb4222ba6a01c17a30fc8
kozhibs <latukhingosha@gmail.com>
2026-07-10 11:25:21 +0300

    added kitty

---
+++ features/parts/kitty.nix
@@ -0,0 +1,9 @@
+{ self, inputs, ... }: {
+  flake.nixosModules.kitty = { pkgs, ... }: {
+    hjem.users.taito.xdg.config.files."kitty/kitty.conf".text = ''
+      hide_window_decorations yes
+      window_border_width 0
+      draw_minimal_borders yes
+    '';
+  }
+}
