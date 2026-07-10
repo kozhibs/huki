c7ee30e2ffdf0cad969e99247d8cc4fe53ff9a42
kozhibs <latukhingosha@gmail.com>
2026-07-10 11:58:10 +0300

    hardware tweaks

---
+++ features/system/hardware.nix
@@ -0,0 +1,30 @@
+{ self, inputs, ... }: {
+  flake.nixosModules.hardware = { pkgs, ... }: {
+    hardware = {
+      opengl.enable = true;
+      graphics = {
+        enable = true;
+        extraPackages = with pkgs; [
+          intel-media-driver
+          vaapiIntel
+          vaapiVdpau
+          libvdpau-va-gl
+        ];
+      };
+    };
+    services = {
+      thermald = {
+        enable = true;
+      };
+      tlp = {
+        enable = true;
+
+        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "balanced";
+
+        CPU_BOOST_ON_AC = 1;
+        CPU_BOOST_ON_BAT = 0;
+      };
+    };
+  };
+}
