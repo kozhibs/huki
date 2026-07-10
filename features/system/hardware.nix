54b86a940347d0162cbd6dc0b7d7b5639ff53df4
kozhibs <latukhingosha@gmail.com>
2026-07-10 12:00:09 +0300

    hardware tweaks

--- features/system/hardware.nix
+++ features/system/hardware.nix
@@ -21,9 +21,6 @@
 
         CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
         CPU_ENERGY_PERF_POLICY_ON_BAT = "balanced";
-
-        CPU_BOOST_ON_AC = 1;
-        CPU_BOOST_ON_BAT = 0;
       };
     };
   };
